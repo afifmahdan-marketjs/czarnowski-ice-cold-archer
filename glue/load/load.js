a4086b17bc47f733500acecf4d07ec03b571f276
afifmahdan <afifudin.mahdan@impact360design.com>
2021-09-28 10:22:45 +0700

    initial project preparation

--- glue/load/load.js
+++ glue/load/load.js
@@ -7,30 +7,30 @@
 */
 
 function loadScriptsSynchronously(arr) {
-    if (!arr || !arr.length) return;
-    var i;
-    var loadFunctions = [];
-    for (i = arr.length - 1; i >= 0; --i) {
-        if (i == arr.length - 1) {
-            loadFunctions[i] = (function (idx) { return function () { jQuery.getScript(arr[idx], function () { }); }; })(i);
-        } else {
-            loadFunctions[i] = (function (idx) { return function () { jQuery.getScript(arr[idx], loadFunctions[idx + 1]); }; })(i);
-        }
-    }
-    loadFunctions[0]();
+	if (!arr || !arr.length) return;
+	var i;
+	var loadFunctions = [];
+	for (i = arr.length - 1; i >= 0; --i) {
+		if (i == arr.length - 1) {
+			loadFunctions[i] = (function (idx) { return function () { jQuery.getScript(arr[idx], function () { }); }; })(i);
+		} else {
+			loadFunctions[i] = (function (idx) { return function () { jQuery.getScript(arr[idx], loadFunctions[idx + 1]); }; })(i);
+		}
+	}
+	loadFunctions[0]();
 }
 
 // if not on server, run compiled version
 loadScriptsSynchronously([
 	// _STRINGS
 	'media/text/strings.js',
-    //"lib/babylon/hand.minified-1.3.7.js",
-    'lib/babylon/pep.js',
-    'lib/babylon/webgl-namespace.js',
-    
-    //"lib/babylon/cannon.js",
-    //"lib/babylon/oimo.js",
-    "lib/babylon/babylon.4.max.js",
+	//"lib/babylon/hand.minified-1.3.7.js",
+	'lib/babylon/pep.js',
+	'lib/babylon/webgl-namespace.js',
+
+	//"lib/babylon/cannon.js",
+	//"lib/babylon/oimo.js",
+	"lib/babylon/babylon.4.2.0.max.js",
 
 	// SETTINGS
 	'settings/dev.js',
@@ -40,7 +40,7 @@ loadScriptsSynchronously([
 	'settings/ad/mobile/preroll/themes/light/ad.js',
 	'settings/ad/mobile/footer/themes/light/ad.js',
 	'settings/ad/mobile/end/themes/light/ad.js',
-				
+
 	// IE >=9 
 	'glue/ie/ie.js',
 
@@ -55,9 +55,9 @@ loadScriptsSynchronously([
 	//Font
 	'glue/font/promise.polyfill.js',
 	'glue/font/fontfaceobserver.standalone.js',
-	
+
 	// Game
 	'lib/impact/impact.js',
 	'lib/game/main.js'
-    
+
 ]);
